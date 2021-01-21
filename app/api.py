6442b4da92eb3fa038f8beeebc78fc5745e946be
alynne <alynneferreiras@gmail.com>
2021-01-20 23:02:17 -0300

    add crud

---
+++ app/api.py
@@ -0,0 +1,54 @@
+from flask import jsonify, request, Response
+
+from settings import app
+from models import DIDNumber
+
+
+@app.route('/')
+@app.route('/dids', methods=['GET'])
+def get_dids():
+    return jsonify({
+        'DID Numbers': DIDNumber.get_all_dids()
+    })
+
+@app.route('/dids/<int:id>', methods=['GET'])
+def get_did_by_id(id):
+    return_value = DIDNumber.get_did(id)
+    return jsonify(return_value)
+
+@app.route('/dids', methods=['POST'])
+def add_did():
+    request_data = request.get_json()
+
+    DIDNumber.add_did(
+        request_data["value"],
+        request_data["monthyPrice"],
+        request_data["setupPrice"],
+        request_data["currency"]
+    )
+
+    response = Response("DID number added", 201, mimetype='application/json')
+    return response
+
+@app.route('/dids/<int:id>', methods=['PUT'])
+def update_did(id):
+    request_data = request.get_json()
+    DIDNumber.update_did(
+        id,
+        request_data["value"],
+        request_data["monthyPrice"],
+        request_data["setupPrice"],
+        request_data["currency"]
+    )
+    response = Response("DID number updated", status=200, mimetype='application/json')
+    return response
+
+@app.route('/dids/<int:id>', methods=['DELETE'])
+def remove_did(id):
+    DIDNumber.delete_did(id)
+    response = Response("DID number deleted", status=200, mimetype='application/json')
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ app/models.py
@@ -0,0 +1,60 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from settings import app
+
+db = SQLAlchemy(app)
+
+
+class DIDNumber(db.Model):
+    __tablename__ = 'didnumber'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    value = db.Column(db.String(17))
+    monthyPrice = db.Column(db.Numeric(scale=2, precision=8, decimal_return_scale=2))
+    setupPrice = db.Column(db.Numeric(scale=2, precision=8, decimal_return_scale=2))
+    currency = db.Column(db.String(5))
+
+    def __init__(self, value, monthyPrice, setupPrice, currency):
+        self.value = value
+        self.monthyPrice = monthyPrice
+        self.setupPrice = setupPrice
+        self.currency = currency
+
+    def __repr__(self):
+        return f'<id {self.id}>'
+
+    def json(self):
+        return {
+            'id': self.id,
+            'value': self.value,
+            'monthyPrice': str(self.monthyPrice),
+            'setupPrice': str(self.setupPrice),
+            'currency': self.currency
+        }
+
+    def add_did(_value, _monthyPrice, _setupPrice, _currency):
+        new_did = DIDNumber(
+            value = _value,
+            monthyPrice = _monthyPrice,
+            setupPrice = _setupPrice,
+            currency = _currency
+        )
+        db.session.add(new_did)
+        db.session.commit()
+
+    def get_all_dids():
+        return [DIDNumber.json(did) for did in DIDNumber.query.all()]
+
+    def get_did(_id):
+        return [DIDNumber.json(DIDNumber.query.filter_by(id=_id).first())]
+
+    def update_did(_id, _value, _monthyPrice, _setupPrice, _currency):
+        did_to_update = DIDNumber.query.filter_by(id=_id).first()
+        did_to_update.value = _value
+        did_to_update.monthyPrice = _monthyPrice
+        did_to_update.setupPrice = _setupPrice
+        did_to_update.currency = _currency
+        db.session.commit()
+
+    def delete_did(_id):
+        DIDNumber.query.filter_by(id=_id).delete()
+        db.session.commit()
