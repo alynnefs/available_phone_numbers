bf6da6de2733978b383528e676df0c07c1d6bbc0
alynne <alynneferreiras@gmail.com>
2021-01-24 23:01:03 -0300

    add some unit tests

--- application/app/unit_tests.py
+++ application/app/unit_tests.py
@@ -45,6 +45,28 @@ class TestAuth(unittest.TestCase):
 
             self.assertEqual("token", keys[0])
 
+    def test_login_empty_user(self):
+        self.credentials = b64encode(b":").decode("utf8")
+        with app.test_client() as client:
+            response = client.get(
+                "/login",
+                headers={
+                    "Authorization": f"Basic {self.credentials}".format()
+                }
+            )
+            self.assertEqual(401, response.status_code)
+
+    def test_login_password_error(self):
+        self.credentials = b64encode(b"example:test123").decode("utf8")
+        with app.test_client() as client:
+            response = client.get(
+                "/login",
+                headers={
+                    "Authorization": f"Basic {self.credentials}".format()
+                }
+            )
+            self.assertEqual(401, response.status_code)
+
     def test_get_all_users(self):
         with app.test_client() as client:
             response = client.get(
@@ -153,6 +175,83 @@ class TestDIDNumber(unittest.TestCase):
                 )
 
             self.assertEqual(response.status_code, 201)
+
+    def test_add_did_negative(self):
+        with app.test_client() as client:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", DeprecationWarning)
+                response = client.post(
+                    '/dids',
+                    data=json.dumps(dict(
+                        value = '11987654321',
+                        monthyPrice = -1.23,
+                        setupPrice = 3.45,
+                        currency = 'U$'
+                    )),
+                    headers={
+                        "Content-Type": "application/json",
+                        "x-access-tokens":self.token
+                    },
+                )
+
+            self.assertEqual(response.status_code, 400)
+
+    def test_add_did_int_value(self):
+        with app.test_client() as client:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", DeprecationWarning)
+                response = client.post(
+                    '/dids',
+                    data=json.dumps(dict(
+                        value = 11987654321,
+                        monthyPrice = 1.23,
+                        setupPrice = 3.45,
+                        currency = 'U$'
+                    )),
+                    headers={
+                        "Content-Type": "application/json",
+                        "x-access-tokens":self.token
+                    },
+                )
+
+            self.assertEqual(response.status_code, 201)
+
+    def test_update_did(self):
+        id = 1
+        with app.test_client() as client:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", DeprecationWarning)
+                response = client.put(
+                    f'/dids/{id}',
+                    data=json.dumps(dict(
+                        id = id,
+                        value = 11987654321,
+                        monthyPrice = 1.23,
+                        setupPrice = 3.45,
+                        currency = 'U$'
+                    )),
+                    headers={
+                        "Content-Type": "application/json",
+                        "x-access-tokens":self.token
+                    },
+                )
+
+            self.assertEqual(response.status_code, 200)
+
+    def test_delete_did(self):
+        id = 5
+        with app.test_client() as client:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", DeprecationWarning)
+                response = client.delete(
+                    f'/dids/{id}',
+                    headers={
+                        "Content-Type": "application/json",
+                        "x-access-tokens":self.token
+                    },
+                )
+
+            self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
